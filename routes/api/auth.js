406d9a5373682cb1308c939c15a8255b6b289556
Julian Vu <julian@uniplanteam.com>
2020-11-17 19:49:05 -0800

    Implement sign-in function in backend

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator"); // Great library for validating inputs
+const bcrypt = require("bcryptjs");
 const auth = require("../../middleware/auth");
 const User = require("../../models/User");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 /**
  * @route         GET api/auth
@@ -17,5 +21,68 @@ router.get("/", auth, async (req, res) => {
     res.status(500).send("GET api/auth - Server Error!");
   }
 });
+
+/**
+ * @route         POST api/auth
+ * @description   Authenticate user and get token
+ * @access        public
+ */
+router.post(
+  "/",
+  [
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Password is required").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      // See if user doesn't exist
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+
+      // Check credentials
+      const isMatched = await bcrypt.compare(password, user.password);
+
+      if (!isMatched) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+
+      // Return JWT
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        process.env.jwtSecret,
+        { expiresIn: 3600 },
+        (err, token) => {
+          if (err) {
+            throw err;
+          } else {
+            res.json({ token });
+          }
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("POST /api/users - Server Error!");
+    }
+  }
+);
 
 module.exports = router;
