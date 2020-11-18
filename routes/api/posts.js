7e111bce79c456d68588b545591d5b6c1d074f58
Julian Vu <julian@uniplanteam.com>
2020-11-18 10:32:51 -0800

    Implement POST request for posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,11 +1,40 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const auth = require("../../middleware/auth");
+const User = require("../../models/User");
+const Post = require("../../models/Post");
 
 /**
- * @route         GET api/posts
- * @description   Test route
- * @access        public
+ * @route         POST api/posts
+ * @description   Create a post
+ * @access        private
  */
-router.get("/", (req, res) => res.send("Posts route"));
+router.post(
+  "/",
+  [auth, [check("text", "Text is required").notEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+
+      const post = await newPost.save();
+      res.json(post);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("POST api/posts - Server Error!");
+    }
+  }
+);
 
 module.exports = router;
