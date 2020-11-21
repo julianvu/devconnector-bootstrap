a4aad85fe0603d5dc9251152245ac7ad5db52a31
Julian Vu <julian@uniplanteam.com>
2020-11-21 15:17:06 -0800

    Prepare for production deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectToDB = require("./db");
+const path = require("path");
 
 // Init app variable with express
 const app = express();
@@ -10,13 +11,24 @@ connectToDB();
 // Init middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("API Running"));
+// Remove this route in production
+// app.get("/", (req, res) => res.send("API Running"));
 
 // Define API routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // Look for environment called PORT or on 5000 locally
 const PORT = process.env.PORT || 5000;
