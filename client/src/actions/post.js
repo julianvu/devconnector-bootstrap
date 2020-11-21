7b8b8e2ed69552487814c0e7bbdf385c5a803b3c
Julian Vu <julian@uniplanteam.com>
2020-11-21 11:59:48 -0800

    Implement Delete post

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,12 +1,29 @@
 import axios from "axios";
 import { setAlert } from "./alert";
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from "./types";
+import { DELETE_POST, GET_POSTS, POST_ERROR, UPDATE_LIKES } from "./types";
 
 // Get posts
 export const getPosts = () => async (dispatch) => {
   try {
     const res = await axios.get("/api/posts");
     dispatch({ type: GET_POSTS, payload: res.data });
+  } catch (error) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status,
+      },
+    });
+  }
+};
+
+// Delete post
+export const deletePost = (id) => async (dispatch) => {
+  try {
+    const res = await axios.delete(`/api/posts/${id}`);
+    dispatch({ type: DELETE_POST, payload: id });
+    dispatch(setAlert("Post Removed", "success"));
   } catch (error) {
     dispatch({
       type: POST_ERROR,
--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,11 +3,12 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import Moment from "react-moment";
-import { addLike, removeLike } from "../../actions/post";
+import { addLike, deletePost, removeLike } from "../../actions/post";
 
 const PostItem = ({
   addLike,
   removeLike,
+  deletePost,
   auth,
   post: { _id, text, name, avatar, user, likes, comments, date },
 }) => {
@@ -19,42 +20,59 @@ const PostItem = ({
     removeLike(postID);
   };
 
+  const onDeleteClick = (postID) => {
+    deletePost(postID);
+  };
+
   return (
-    <div className="post bg-white p-2 my-2">
-      <div>
+    <div className="post bg-white py-4 my-2 border rounded-lg row align-items-center">
+      <div className="col-md-3 text-center">
         <Link to="/profile">
           <img src={avatar} alt="" className="rounded-circle" />
-          <h4>{name}</h4>
+          <h4 className="m-0">{name}</h4>
         </Link>
       </div>
-      <div>
-        <p className="my-2">{text}</p>
-        <p>
-          Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
-        </p>
-        <button onClick={() => onLikeClick(_id)} className="btn btn-light">
-          <i className="fas fa-thumbs-up"></i>
+      <div className="col-md-9 align-self-end">
+        <p>{text}</p>
+        <div>
+          <small className="text-muted">
+            Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
+          </small>
+          <div className="d-flex">
+            <button
+              onClick={() => onLikeClick(_id)}
+              className="btn btn-light btn-like-unlike mr-2"
+            >
+              <i className="fas fa-thumbs-up"></i>
 
-          <span>
-            {likes.length > 0 && (
-              <span className="badge badge-light">{likes.length}</span>
+              <span>
+                {likes.length > 0 && (
+                  <span className="badge badge-light">{likes.length}</span>
+                )}
+              </span>
+            </button>
+            <button
+              onClick={() => onUnlikeClick(_id)}
+              className="btn btn-light btn-like-unlike mr-2"
+            >
+              <i className="fas fa-thumbs-down"></i>
+            </button>
+            <Link to={`/post/${_id}`} className="btn btn-primary mr-2">
+              Discussion{" "}
+              {comments.length > 0 && (
+                <span className="badge badge-light">{comments.length}</span>
+              )}
+            </Link>
+            {!auth.loading && user === auth.user._id && (
+              <button
+                onClick={() => onDeleteClick(_id)}
+                className="btn btn-danger"
+              >
+                <i className="fas fa-times"></i>
+              </button>
             )}
-          </span>
-        </button>
-        <button onClick={() => onUnlikeClick(_id)} className="btn btn-light">
-          <i className="fas fa-thumbs-down"></i>
-        </button>
-        <Link to={`/post/${_id}`} className="btn btn-primary">
-          Discussion{" "}
-          {comments.length > 0 && (
-            <span className="badge badge-light">{comments.length}</span>
-          )}
-        </Link>
-        {!auth.loading && user === auth.user._id && (
-          <button className="btn btn-danger">
-            <i className="fas fa-times"></i>
-          </button>
-        )}
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -63,10 +81,15 @@ const PostItem = ({
 PostItem.propTypes = {
   post: PropTypes.object.isRequired,
   auth: PropTypes.object.isRequired,
+  addLike: PropTypes.func.isRequired,
+  removeLike: PropTypes.func.isRequired,
+  deletePost: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   auth: state.auth,
 });
 
-export default connect(mapStateToProps, { addLike, removeLike })(PostItem);
+export default connect(mapStateToProps, { addLike, removeLike, deletePost })(
+  PostItem
+);
