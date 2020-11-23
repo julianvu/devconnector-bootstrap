f79d579b2ed0f41e68a4fc5216761e2226ac2999
Julian Vu <julian@uniplanteam.com>
2020-11-23 10:29:49 -0800

    Log out user on expired token

---
+++ client/src/utils/tokenExpire.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+import store from "../store";
+import { LOGOUT } from "../actions/types";
+
+const api = axios.create({
+  baseURL: "/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Intercept error responses from api to check if token has expired
+// Log out if expired or invalid token
+api.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    if (err.response.status === 401) {
+      store.dispatch({ type: LOGOUT });
+    }
+    return Promise.reject(err);
+  }
+);
