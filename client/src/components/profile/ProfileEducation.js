e1158a40953ab5c083998a5e04d7034ccb1859d0
Julian Vu <julian@uniplanteam.com>
2020-11-24 20:29:12 -0800

    Refactor-remove Moment from ProfileEducation

--- client/src/components/profile/ProfileEducation.js
+++ client/src/components/profile/ProfileEducation.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Moment from "react-moment";
+import formatDate from "../../utils/formatDate";
 
 const ProfileEducation = ({
   education: { school, degree, fieldofstudy, current, to, from, description },
@@ -8,8 +9,7 @@ const ProfileEducation = ({
   <div>
     <h4 className="text-dark">{school}</h4>
     <p>
-      <Moment format="YYYY/MM/DD">{from}</Moment> -{" "}
-      {!to ? " Current" : <Moment format="YYYY/MM/DD">{to}</Moment>}
+      {formatDate(from)} - {to && !current ? formatDate(to) : "Current"}
     </p>
     <p>
       <strong>Degree: </strong> {degree}
